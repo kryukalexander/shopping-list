8f1ac734480bdcf4ee976ac86e590ce05f640564
Alexander Kryuk <kryukalexander@gmail.com>
2018-02-16 16:26:08 +0600

    first commit

---
+++ src/components/ListItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="list-item">
+        <input type="checkbox" v-model="item.selected">
+        <span>{{ item.name }}</span>
+        <span v-if="item.count">{{item.count }}</span>
+        <span v-if="item.units">{{item.units }}</span>
+        <button v-on:click="onRemove(item)">X</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "list-item",
+        props: ["item", "onRemove"],
+    }
+</script>
+
+<style scoped>
+    .list-item {
+        background-color: #f1f1f1;
+        padding: 5px;
+    }
+</style>
