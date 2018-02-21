32b7e9432d5535b66492fc988248dbdd72cb870e
Alexander Kryuk <kryukalexander@gmail.com>
2018-02-21 17:18:10 +0600

    Firebase auth WIP

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ router.beforeEach((to, from, next) => {
     let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
     if (requiresAuth && !currentUser) next('login');
-    else if (!requiresAuth && currentUser) next('hello');
+    else if (!requiresAuth && currentUser) next('list');
     else next()
 });
 
