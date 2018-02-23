cf5179583d62541379b08405dca8be00074b64f5
Alexander Kryuk <kryukalexander@gmail.com>
2018-02-23 16:11:22 +0600

    Login form styling

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="login">
-        <input type="text" v-model="email">
-        <input type="text" v-model="password">
-        <button @click="login()">Login</button>
+        <div class="login__form">
+            <input type="text" v-model="email">
+            <input type="text" v-model="password">
+            <button @click="login()">Login</button>
+        </div>
     </div>
 </template>
 
@@ -29,7 +31,7 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
     .login {
         background-color: #f2f2f2;
         height: 100%;
@@ -37,5 +39,23 @@
         display: flex;
         align-items: center;
         justify-content: center;
+
+        &__form {
+            width: 500px;
+            max-width: 100%;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.15);
+
+            * {
+                display: block;
+                width: 100%;
+
+                &:not(:last-child) {
+                    margin-bottom: 10px;
+                }
+            }
+        }
     }
 </style>
