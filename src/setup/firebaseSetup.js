5044cb18115cf86fbbd760989cfc6d04e8f6fdd5
Alexander Kryuk <kryukalexander@gmail.com>
2018-08-27 13:10:10 +0600

    Removed whitespace

--- src/setup/firebaseSetup.js
+++ src/setup/firebaseSetup.js
@@ -2,7 +2,6 @@ import firebase from 'firebase';
 import config from '../config';
 
 export const isProd = process.env.NODE_ENV === 'production';
-
 const connection = firebase.initializeApp(config);
 const db = connection.database();
 export const cartRef = isProd ? db.ref('cart') :  db.ref('cart-test');
